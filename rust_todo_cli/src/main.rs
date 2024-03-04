6ed4db69dbed320d59a3572f14901afcefff9c2e
li lx <llxclaude126@gmail.com>
2024-03-04 09:33:56 -0800

    implemented the function for listing all the current tasks we have in the task list

--- rust_todo_cli/src/main.rs
+++ rust_todo_cli/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
+// this is the struct to store hte task content and the task id
+struct Task {
+    id: usize,
+    content: String,
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
+
+// add one task to the task list
+fn add_task(tasks: &mut Vec<Task>, content: String) {
+    let id = tasks.len() + 1; // get the current task id by increase the current max id by 1
+    tasks.push(Task {id, content}); // push the new task to the task list
+}
+
+// list all the tasks in the task list (by printing on the console)
+fn list_tasks(tasks: &mut Vec<Task>) {
+    for task in tasks {
+        println!("{}: {}", task.id, task.content);
+    }
+}
+
